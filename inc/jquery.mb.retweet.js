e0516baa7f3b1e523c0ce0365c52accaadffc416
pupunzi <matteo@open-lab.com>
2011-07-04 12:57:38 +0200

    various update

--- inc/jquery.mb.retweet.js
+++ inc/jquery.mb.retweet.js
@@ -12,7 +12,7 @@
 /*
  * jQuery.mb.components: jquery.mb.retweet
  * version: 1.3
- * © 2001 - 2009 Matteo Bicocchi (pupunzi), Open Lab
+ * © 2001 - 2011 Matteo Bicocchi (pupunzi), Open Lab
  *
  */
 
